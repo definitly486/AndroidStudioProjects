cecb955a9e4d2ff127511759257621063375eb0f
Your Name <you@example.com>
2025-08-07 09:49:45 +0300

    изменено:      Decrypt/app/src/main/java/com/example/decrypt/MainActivity.kt 	изменено:      Decrypt/app/src/main/java/com/example/decrypt/decrypted.kt 	переименовано: Decrypt/app/src/main/java/com/example/decrypt/derypt.kt -> Decrypt/app/src/main/java/com/example/decrypt/decryptfile.kt 	новый файл:    Decrypt/app/src/main/java/com/example/decrypt/makeCipher.kt

--- Decrypt/app/src/main/java/com/example/decrypt/decryptfile.kt
+++ Decrypt/app/src/main/java/com/example/decrypt/decryptfile.kt
@@ -1,22 +1,13 @@
+@file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+
 package com.example.decrypt
 
-import android.annotation.SuppressLint
-import com.example.decrypt.FileEncryptor.makeCipher
-import java.io.BufferedReader
-import java.io.FileInputStream
-import java.io.InputStreamReader
-import java.security.spec.KeySpec
-import javax.crypto.Cipher
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
 import java.security.GeneralSecurityException
-import java.util.Arrays
-import javax.crypto.CipherInputStream
-import javax.crypto.SecretKeyFactory
-import javax.crypto.spec.IvParameterSpec
-import javax.crypto.spec.PBEKeySpec
-import javax.crypto.spec.SecretKeySpec
+
 /**Decrypts one file to a second file using a key derived from a passphrase: */
 @Throws(GeneralSecurityException::class, IOException::class)
 fun decryptFile(fileName: String?, pass: String) {
@@ -25,7 +16,7 @@ fun decryptFile(fileName: String?, pass: String) {
     val inFile = File(fileName)
 
     //Generate the cipher using pass:
-    val cipher = makeCipher(pass, false)
+    val cipher = makeCipherver2(pass, false)
 
     //Read in the file:
     val inStream = FileInputStream(inFile)
@@ -42,12 +33,12 @@ fun decryptFile(fileName: String?, pass: String) {
     //this at the end
     //For robust check, check that padCount bytes at the end have same value
     if (padCount >= 1 && padCount <= 8) {
-        decData = Arrays.copyOfRange(decData, 0, decData.size - padCount)
+        decData = decData.copyOfRange(0, decData.size - padCount)
     }
 
 
     //Write the decrypted data to a new file:
-    val target = FileOutputStream(File(fileName + ".decrypted.txt"))
+    val target = FileOutputStream(File("$fileName.decrypted.txt"))
     target.write(decData)
     target.close()
 }
