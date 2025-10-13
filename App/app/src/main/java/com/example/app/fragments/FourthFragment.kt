3c3ef02ed2dd3a0f7ca1ebc44539942faec3ed69
Your Name <you@example.com>
2025-10-14 01:33:01 +0300

    новый файл:    App/.idea/compiler.xml 	изменено:      App/.idea/deploymentTargetSelector.xml 	изменено:      App/.idea/misc.xml 	изменено:      App/app/build.gradle.kts 	изменено:      App/app/src/main/java/com/example/app/fragments/FourthFragment.kt 	изменено:      App/app/src/main/res/layout/fragment_fourh.xml

--- App/app/src/main/java/com/example/app/fragments/FourthFragment.kt
+++ App/app/src/main/java/com/example/app/fragments/FourthFragment.kt
@@ -1,24 +1,55 @@
 package com.example.app.fragments
-
-
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.app.DownloadManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.app.R
-
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.util.encoders.Base64
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayInputStream
+import java.io.InputStreamReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.security.Security
+import java.util.jar.Manifest
+import javax.crypto.Cipher
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.PBEKeySpec
+import javax.crypto.spec.SecretKeySpec
 
 class FourthFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_fourh, container, false)
 
+    private lateinit var downloadManager: DownloadManager
+    private var myDownloadID: Long = 0
+    private val apkHttpUrl = "https://github.com/definitly486/definitly486/releases/download/shared/"
+
+    private var inputFileUri: Uri? = null
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_fourh, container, false)
         return view
     }
 }
+
 
