dda1b58964438a7d67253512b6b185ea720f560b
Your Name <you@example.com>
2025-10-20 19:45:11 +0300

    изменено:      App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt 	изменено:      App_Redmi_A5/app/src/main/res/layout/fragment_first.xml 	изменено:      App_Redmi_A5/app/src/main/res/values/strings.xml

--- App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt
+++ App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt
@@ -133,6 +133,18 @@ class FirstFragment : Fragment() {
             }
         }
 
+        val installtelegram = view.findViewById<Button>(R.id.installtelegram)
+        installtelegram.setOnClickListener {
+            val apkUrl1 = "https://github.com/definitly486/redmia5/releases/download/apk/Telegram+11.14.1.apk"
+            downloadHelper.download(apkUrl1) { file ->
+                if (file != null) {
+                    Toast.makeText(requireContext(), "Файл загружен: ${file.name}", Toast.LENGTH_SHORT).show()
+                    // Установка происходит автоматически после завершения
+                } else {
+                    Toast.makeText(requireContext(), "Ошибка загрузки", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
         return view
     }
