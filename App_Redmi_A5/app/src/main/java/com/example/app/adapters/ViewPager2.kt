121e3b1f7f33c54d0434c835f942a2c46a502400
Your Name <you@example.com>
2025-10-20 15:29:26 +0300

    новый файл:    App_Redmi_A5/.gitignore 	новый файл:    App_Redmi_A5/.idea/.gitignore 	новый файл:    App_Redmi_A5/.idea/.name 	новый файл:    App_Redmi_A5/.idea/AndroidProjectSystem.xml

---
+++ App_Redmi_A5/app/src/main/java/com/example/app/adapters/ViewPager2.kt
@@ -0,0 +1,20 @@
+package com.example.app.adapters
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.app.fragments.FirstFragment
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> FirstFragment()
+
+
+            else -> throw IllegalArgumentException("Invalid position")
+        }
+    }
+
+    override fun getItemCount(): Int = 2
+}
