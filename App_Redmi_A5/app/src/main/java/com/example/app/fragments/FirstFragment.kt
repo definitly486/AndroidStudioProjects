d25bbd57a72222a0d959be62d2b3cc968852c9bb
Your Name <you@example.com>
2025-10-21 03:24:35 +0300

    изменено:      App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt 	изменено:      App_Redmi_A5/app/src/main/java/com/example/app/fragments/SecondFragment.kt 	изменено:      App_Redmi_A5/app/src/main/res/layout/fragment_first.xml 	изменено:      App_Redmi_A5/app/src/main/res/values/strings.xml

--- App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt
+++ App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt
@@ -199,6 +199,20 @@ class FirstFragment : Fragment() {
             }
         }
 
+        val installgesture = view.findViewById<Button>(R.id.installgesture)
+        installgesture.setOnClickListener {
+            val apkUrl1 = "https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/Gesture.apk"
+            downloadHelper.download(apkUrl1) { file ->
+                if (file != null) {
+                    Toast.makeText(requireContext(), "Файл загружен: ${file.name}", Toast.LENGTH_SHORT).show()
+                    // Установка происходит автоматически после завершения
+                } else {
+                    Toast.makeText(requireContext(), "Ошибка загрузки", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+
         return view
     }
 
