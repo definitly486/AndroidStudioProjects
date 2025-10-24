e2ebdaf60f93d34ddc898a5fa5a33a8b4ac29bac
Your Name <you@example.com>
2025-10-24 14:34:16 +0300

    изменено:      App_Redmi_A5/app/src/main/java/com/example/app/fragments/SecondFragment.kt

--- App_Redmi_A5/app/src/main/java/com/example/app/fragments/SecondFragment.kt
+++ App_Redmi_A5/app/src/main/java/com/example/app/fragments/SecondFragment.kt
@@ -36,6 +36,7 @@ class SecondFragment : Fragment() {
 
         // Инициализация DownloadHelper
         downloadHelper = DownloadHelper(requireContext())
+        downloadHelper2 = DownloadHelper2(requireContext())
 
         // Настройка кнопок
         setupButtons(view)
@@ -104,6 +105,7 @@ class SecondFragment : Fragment() {
 
     private fun downloadKSUZip() {
         downloadHelper.downloadgpg("https://github.com/definitly486/redmia5/releases/download/root/APatch-KSU.zip")
+        Thread.sleep(5000L)
         downloadHelper2.copyKSUZip()
     }
 
