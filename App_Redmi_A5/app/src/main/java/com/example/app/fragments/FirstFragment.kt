2d779e4a7f09d06aea9be928faf81b85d0e4bb0c
Your Name <you@example.com>
2025-10-24 15:13:38 +0300

    изменено:      App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt 	изменено:      App_Redmi_A5/app/src/main/res/layout/fragment_first.xml 	изменено:      App_Redmi_A5/app/src/main/res/values/strings.xml

--- App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt
+++ App_Redmi_A5/app/src/main/java/com/example/app/fragments/FirstFragment.kt
@@ -73,7 +73,8 @@ class FirstFragment : Fragment() {
             R.id.installtermosplus to "https://github.com/definitly486/redmia5/releases/download/apk/com.termoneplus_3.6.0.apk",
             R.id.installapatch to "https://github.com/definitly486/redmia5/releases/download/apk/APatch_11107_11107-release-signed.apk",
             R.id.installkernelsu to "https://github.com/definitly486/redmia5/releases/download/apk/KernelSU_v1.0.5_12081-release.apk",
-            R.id.installcore to "https://github.com/definitly486/redmia5/releases/download/apk/Core+Music+Player_1.0.apk"
+            R.id.installcore to "https://github.com/definitly486/redmia5/releases/download/apk/Core+Music+Player_1.0.apk",
+            R.id.installpluma to "https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/Pluma_.private_fast.browser_1.80_APKPure.apk"
         )
 
         // Назначаем обработчик события каждому элементу карты
