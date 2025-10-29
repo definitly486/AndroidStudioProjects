42cef0b8e4e5e6970838091418a322834d6a1915
Your Name <you@example.com>
2025-10-29 11:33:08 +0300

    изменено:      App_Redmi_A5/app/src/main/java/com/example/app/DecryptAndExtract.kt

--- App_Redmi_A5/app/src/main/java/com/example/app/DecryptAndExtract.kt
+++ App_Redmi_A5/app/src/main/java/com/example/app/DecryptAndExtract.kt
@@ -9,12 +9,16 @@ import android.net.Uri
 import android.os.Environment
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
+import com.example.app.fragments.RootChecker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
 import java.lang.ProcessBuilder
 import java.lang.RuntimeException
 
@@ -79,6 +83,77 @@ suspend fun decryptAndExtractArchive(context: Context, password: String) {
         withContext(Dispatchers.Main) {
             showToastOnMainThread(context, "Ошибка при установке: ${e.message}")
         }
+    }
+}
+
+fun copyprofile(context: Context) {
+
+
+    fun showCompletionDialoginstall() {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Проверка root")
+        builder.setMessage("Root доступ отсуствует,приложения не будут установлены")
+        builder.setPositiveButton("Продолжить") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
+
+    if (RootChecker.hasRootAccess(context)) {
+
+        Toast.makeText(context, "Устройство имеет root-доступ.", Toast.LENGTH_SHORT)
+            .show()
+    } else {
+        showCompletionDialoginstall()
+        return
+    }
+
+    Toast.makeText(context, "Копируем redmia5-main ...", Toast.LENGTH_SHORT).show()
+
+    val ownerCmd =
+        "su - root -c   ls -l   /data_mirror/data_ce/null/0/com.termos | awk '{print $3}' | head -n 2"
+    val fileOwner = execShell(ownerCmd)?.trim() ?: ""
+
+    val commands = arrayOf(
+
+        "su - root -c cp  -R /storage/emulated/0/Android/data/com.example.app/files/Download/redmia5-main /data_mirror/data_ce/null/0/com.termos/files/home",
+        "su - root -c chmod -R 0755 /data_mirror/data_ce/null/0/com.termos/files/home",
+        "su - root -c chown -R  $fileOwner:$fileOwner /data_mirror/data_ce/null/0/com.termos/files/home/redmia5-main"
+    )
+
+    var process: Process?
+
+    for (command in commands) {
+        process = Runtime.getRuntime().exec(command)
+        process.waitFor() // Wait for the command to finish
+        if (process.exitValue() != 0) {
+            Toast.makeText(context, "Ошибка при копирование main: $command", Toast.LENGTH_LONG)
+                .show()
+            return
+        }
+    }
+    Toast.makeText(context, "Копирование  main завершенo", Toast.LENGTH_SHORT).show()
+}
+
+private fun execShell(cmd: String): String? {
+    try {
+        val process = Runtime.getRuntime().exec(cmd)
+        process.waitFor()
+        if (process.exitValue() != 0) {
+            throw Exception("Ошибка при выполнении команды: $cmd")
+        }
+
+        val outputStream = BufferedReader(InputStreamReader(process.inputStream))
+        val resultBuilder = StringBuilder()
+        while (true) {
+            val line = outputStream.readLine() ?: break
+            resultBuilder.append(line).append("\n")
+        }
+        return resultBuilder.toString().trim()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
     }
 }
 
