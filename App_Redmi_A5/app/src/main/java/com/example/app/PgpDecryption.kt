5e49895e611b4ad0dfcb85838eaf6e991ad89d44
Your Name <you@example.com>
2025-11-14 10:30:37 +0300

    изменено:      App_Redmi_A5/app/src/main/java/com/example/app/DecryptAndExtract.kt 	изменено:      App_Redmi_A5/app/src/main/java/com/example/app/PgpDecryption.kt

--- App_Redmi_A5/app/src/main/java/com/example/app/PgpDecryption.kt
+++ App_Redmi_A5/app/src/main/java/com/example/app/PgpDecryption.kt
@@ -1,8 +1,10 @@
 @file:Suppress("SpellCheckingInspection")
 
+
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
 
+@Suppress("unused")
 fun decryptPGPFileWithPassword(context: Context){
 
     val builder = AlertDialog.Builder(context)
