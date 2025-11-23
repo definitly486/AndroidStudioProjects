c4e76d5604957902f2e71a655039a63bbf452906
Your Name <you@example.com>
2025-11-24 02:03:23 +0300

    изменено:      App_Redmi_A5/app/src/main/java/com/example/app/fragments/DownloadHelper2.kt

--- App_Redmi_A5/app/src/main/java/com/example/app/fragments/DownloadHelper2.kt
+++ App_Redmi_A5/app/src/main/java/com/example/app/fragments/DownloadHelper2.kt
@@ -179,15 +179,29 @@ class DownloadHelper2(private val context: Context) {
             return
         }
 
+val pipScript = """
+    #!/system/bin/sh
+    source /data/local/tmp/env/bin/activate
+    pip "$@"
+""".trimIndent()
+
+try {
+    val file = File("$folder/pip")
+    file.writeText(pipScript)
+    file.setExecutable(true)  // chmod +x
+    Toast.makeText(context, "pip успешно создан!", Toast.LENGTH_SHORT).show()
+} catch (e: Exception) {
+    Toast.makeText(context, "Ошибка: ${e.message}", Toast.LENGTH_LONG).show()
+}
+
+
         Toast.makeText(context, "Установка pip Python3 ...", Toast.LENGTH_SHORT).show()
         val commands = arrayOf(
-            "su - root -c mount -o rw,remount /system",
-            "su -c 'mkdir -p /data/local/tmp'",
-            "su -c 'cat > /data/local/tmp/pip <<\"EOF\"\n#!/system/bin/sh\nsource /data/local/tmp/env/bin/activate 2>/dev/null || exit 1\nexec pip \"\$@\"\nEOF'",
-            "su -c 'chmod 755 /data/local/tmp/pip'",
-            "su - root -c cp /data/local/tmp/pip /system/bin",
-            "su - root -c chmod +x /system/bin/pip",
-            "su - root -c chmod 0755 /system/bin/pip"
+          //  "su - root -c mount -o rw,remount /system",
+"su echo"
+         //"su - root -c cp /data/local/tmp/pip /system/bin",
+            //"su - root -c chmod +x /system/bin/pip",
+            //"su - root -c chmod 0755 /system/bin/pip"
 
         )
 
